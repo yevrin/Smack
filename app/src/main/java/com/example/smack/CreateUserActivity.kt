8c579d791b8755268137058807c1671ff186e4cd
yevrin <klkite@uwaterloo.ca>
2019-03-18 12:41:28 -0400

    lesson-05

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profiledefault"
+    var avatarColour = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +18,18 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateBGColorBtnCreateClicked(view : View){
+        val random = Random()
+        val red = random.nextInt(256)
+        val green = random.nextInt(256)
+        val blue = random.nextInt(256)
+
+        avatarImgCreate.setBackgroundColor(Color.rgb(red, green, blue))
+
+        val redPercent = red.toDouble() / 255
+        val greenPercent = green.toDouble() / 255
+        val bluePercent = blue.toDouble() / 255
+
+        avatarColour = "[$redPercent, $greenPercent, $bluePercent, 1]"
 
     }
 
@@ -20,6 +38,21 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateAvatarClicked(view : View){
+
+        val random = Random()
+        val whatHue = random.nextInt(2)
+        val whatImg = random.nextInt(28)
+
+        if(whatHue == 0){
+            userAvatar="light$whatImg"
+        }else{
+            userAvatar= "dark$whatImg"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        avatarImgCreate.setImageResource(resourceId)
+
+
 
     }
 }
